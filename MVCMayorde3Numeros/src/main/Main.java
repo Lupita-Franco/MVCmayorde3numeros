1cd3b0a074272bc07abb8633dc5f27ee7515d7e1
luis-alberto-islas <31744077+luis-alberto-islas@users.noreply.github.com>
2017-09-29 16:09:15 -0500

    Update Main.java

--- MVCMayorde3Numeros/src/main/Main.java
+++ MVCMayorde3Numeros/src/main/Main.java
@@ -1,6 +1,17 @@
-
 package main;
 
 
+import models.ModelMenor;
+import views.ViewMenor;
+import controllers.ControllerMenor;
+
+
 public class Main {
+    
+    public static void main (String mvc[]){
+    ModelMenor model_menor = new ModelMenor();
+    ViewMenor view_menor = new ViewMenor();
+    ControllerMenor controller_menor = new ControllerMenor(model_menor,view_menor);
+    }
+
 }
